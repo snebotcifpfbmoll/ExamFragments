dabfd033134257ebce7285b9acafd9cb7ce58826
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2020-12-03 17:12:37 +0100

    cleanup

--- app/src/main/java/com/serafinebot/dint/examfragments/TopFragment.java
+++ app/src/main/java/com/serafinebot/dint/examfragments/TopFragment.java
@@ -11,11 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TopFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TopFragment extends Fragment {
     private static final String TAG = "fragment_top";
 
@@ -23,46 +18,6 @@ public class TopFragment extends Fragment {
     private SeekBar seekBar = null;
     private Button changeTextButton = null;
     public FragmentDelegate delegate = null;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TopFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_top.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TopFragment newInstance(String param1, String param2) {
-        TopFragment fragment = new TopFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/serafinebot/dint/examfragments/TwoFragment.java
+++ app/src/main/java/com/serafinebot/dint/examfragments/TwoFragment.java
@@ -16,55 +16,9 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TwoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TwoFragment extends Fragment {
     private static final String TAG = "TwoFragment";
-
     private TextView textView = null;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TwoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_two.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TwoFragment newInstance(String param1, String param2) {
-        TwoFragment fragment = new TwoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
